8a3aa9bb872a6855052f7189046f2c1fbabf773d
kaichong lin <coco.keven@gmail.com>
2024-02-14 22:30:42 -0600

    remove .vscode folder, learning notes added to readme.md, un-used imports deleted.

--- learning/src/main/java/dev/learning/ReviewController.java
+++ learning/src/main/java/dev/learning/ReviewController.java
@@ -2,13 +2,11 @@ package dev.learning;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- learning/src/main/java/dev/learning/ReviewService.java
+++ learning/src/main/java/dev/learning/ReviewService.java
@@ -1,7 +1,5 @@
 package dev.learning;
 
-import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
